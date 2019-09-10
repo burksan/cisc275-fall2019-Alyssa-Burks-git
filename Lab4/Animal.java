4333e1acd2c485ce0e5db4eac93bd4873bcb4858
Alyssa Burks <Alyssa@wifi-roaming-128-4-156-244.host.udel.edu>
2019-09-10 16:22:22 -0400

    modified Animal w/getters and setters

--- Lab4/Animal.java
+++ Lab4/Animal.java
@@ -1,4 +1,16 @@
 public class Animal{
 	public String name;
 	public int legs;
+	
+	public String getName(){
+		return name;}
+	
+	public void setName(){
+		this.name = name;
+	
+	public String getLegs(){
+		return legs;
+		
+	public void setLegs(){
+		this.legs = legs;
 	}
